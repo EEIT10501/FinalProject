4593858bf8231b1220b427df75ae892e08993d5c
石偉廷 <sam810331@gmail.com>
2019-02-21 22:33:46 +0800

    完成 product message永續類別

--- FunWorkProject2019/src/main/java/com/funwork/model/Product.java
+++ FunWorkProject2019/src/main/java/com/funwork/model/Product.java
@@ -1,5 +1,6 @@
 package com.funwork.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,6 +10,12 @@ import javax.persistence.Id;
 public class Product {
 
 	private Integer productId;
+	private String name;
+	private String description;
+	private Double price;
+
+	public Product() {
+	}
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,6 +25,32 @@ public class Product {
 
 	public void setProductId(Integer productId) {
 		this.productId = productId;
+	}
+
+	@Column(columnDefinition = "nvarchar(255)")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(columnDefinition = "nvarchar(255)")
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
 	}
 
 }
