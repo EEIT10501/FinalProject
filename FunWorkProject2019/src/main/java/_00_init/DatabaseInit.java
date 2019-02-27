92f7d59705534892697d6e782862b28bb5d99ecd
石偉廷 <sam810331@gmail.com>
2019-02-27 22:32:37 +0800

    完成Product、Order、Message測試資料新增程式

--- FunWorkProject2019/src/main/java/_00_init/DatabaseInit.java
+++ FunWorkProject2019/src/main/java/_00_init/DatabaseInit.java
@@ -2,7 +2,10 @@ package _00_init;
 
 import org.hibernate.SessionFactory;
 
+import _00_init.tables.MessageTableInit;
 import _00_init.tables.NotificationTableInit;
+import _00_init.tables.OrderTableInit;
+import _00_init.tables.ProductTableInit;
 import _00_init.util.HibernateUtils;
 
 public class DatabaseInit {
@@ -11,7 +14,13 @@ public class DatabaseInit {
 
 		SessionFactory factory = HibernateUtils.getSessionFactory();
 
+		// 需要先有User
 		new NotificationTableInit(factory).initNotificaion();
+		new ProductTableInit(factory).initProduct();
+		// 需要先有User、Product
+		new OrderTableInit(factory).initOrder();
+		// 需要先有User、Application
+		new MessageTableInit(factory).initMessage();
 
 		factory.close();
 	}
---
+++ FunWorkProject2019/src/main/java/_00_init/tables/ProductTableInit.java
@@ -0,0 +1,51 @@
+package _00_init.tables;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import com.funwork.model.Product;
+
+public class ProductTableInit {
+
+	SessionFactory factory;
+	String line = "";
+	public static final String UTF8_BOM = "\uFEFF";
+
+	public ProductTableInit(SessionFactory factory) {
+		this.factory = factory;
+	}
+
+	public void initProduct() {
+
+		Session session = factory.getCurrentSession();
+		Transaction tx = null;
+
+		try (FileReader fr = new FileReader("data/Product.dat"); BufferedReader br = new BufferedReader(fr);) {
+			tx = session.beginTransaction();
+			while ((line = br.readLine()) != null) {
+				if (line.startsWith(UTF8_BOM)) {
+					line = line.substring(1);
+				}
+				String[] token = line.split("\\|");
+				String name = token[0];
+				String description = token[1];
+				String price = token[2];
+				Product product = new Product();
+				product.setName(name);
+				product.setDescription(description);
+				product.setPrice(Double.valueOf(price));
+				session.save(product);
+			}
+			tx.commit();
+			System.out.println("Product資料新增成功");
+		} catch (Exception e) {
+			System.err.println("新建Product表格時發生例外: " + e.getMessage());
+			e.printStackTrace();
+			tx.rollback();
+		}
+	}
+}
